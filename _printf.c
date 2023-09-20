7e3ec861c11bc45b40db168a25b731e3fe12c4f6
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2023-09-20 01:20:19 -0700

    updating printf file

--- _printf.c
+++ _printf.c
@@ -1,37 +1,47 @@
-#include <stdarg.h>
-#include <unistd.h>
 #include "main.h"
 
 /**
- * _printf - Custom printf function
- * @format: The format string
- * @...: Variable number of arguments
- *
- * Return: Number of characters printed (excluding null byte)
+ * _printf - is a function that selects the correct function to print.
+ * @format: identifier to look for.
+ * Return: the length of the string.
  */
 
-int _printf(const char *format, ...)
+int _printf(const char * const format, ...)
 {
+	convert_match m[] = {
+		{"%s", printf_string}, {"%c", printf_char},
+		{"%%", printf_37},
+		{"%i", printf_int}, {"%d", printf_dec}, {"%r", printf_srev},
+		{"%R", printf_rot13}, {"%b", printf_bin}, {"%u", printf_unsigned},
+		{"%o", printf_oct}, {"%x", printf_hex}, {"%X", printf_HEX},
+		{"%S", printf_exclusive_string}, {"%p", printf_pointer}
+	};
+
 	va_list args;
-	int char_count = 0;
+	int i = 0, j, len = 0;
 
 	va_start(args, format);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
 
-	while (*format)
+Here:
+	while (format[i] != '\0')
 	{
-		if (*format == '%' && *(format + 1) != '\0')
+		j = 13;
+		while (j >= 0)
 		{
-			format++; /* Move past '%' */
-			char_count += handle_format_specifier(*format, args);
+			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
+			{
+				len += m[j].f(args);
+				i = i + 2;
+				goto Here;
+			}
+			j--;
 		}
-		else
-		{
-			write(1, format, 1); /* Print non-% character */
-			char_count++;
-		}
-		format++; /* Move to the next character */
+		_putchar(format[i]);
+		len++;
+		i++;
 	}
-
 	va_end(args);
-	return (char_count);
+	return (len);
 }
